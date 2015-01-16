ef899e3abe80e5b386d35331a3868a863aaf9945
xiaosong <song_shu@163.com>
2015-01-16 16:12:16 +0800

    commit binarySearch

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,21 +1,35 @@
+import java.io.File;
 import java.util.Arrays;
 
 import edu.princeton.cs.introcs.In;
 import edu.princeton.cs.introcs.StdIn;
 import edu.princeton.cs.introcs.StdOut;
 
-/**
- * The <tt>BinarySearch</tt> class provides a static method for binary searching for an integer in a sorted array of
- * integers.
- * <p>
- * The <em>rank</em> operations takes logarithmic time in the worst case.
- * <p>
- * For additional documentation, see <a href="http://algs4.cs.princeton.edu/11model">Section 1.1</a> of <i>Algorithms,
- * 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- * 
- * @author Robert Sedgewick
- * @author Kevin Wayne
- */
+/*************************************************************************
+ *  Compilation:  javac BinarySearch.java
+ *  Execution:    java BinarySearch whitelist.txt < input.txt
+ *  Dependencies: In.java StdIn.java StdOut.java
+ *  Data files:   http://algs4.cs.princeton.edu/11model/tinyW.txt
+ *                http://algs4.cs.princeton.edu/11model/tinyT.txt
+ *                http://algs4.cs.princeton.edu/11model/largeW.txt
+ *                http://algs4.cs.princeton.edu/11model/largeT.txt
+ *
+ *  % java BinarySearch tinyW.txt < tinyT.txt
+ *  50
+ *  99
+ *  13
+ *
+ *  % java BinarySearch largeW.txt < largeT.txt | more
+ *  499569
+ *  984875
+ *  295754
+ *  207807
+ *  140925
+ *  161828
+ *  [367,966 total values]
+ *  
+ *************************************************************************/
+
 public class BinarySearch
 {
 
@@ -52,19 +66,19 @@ public class BinarySearch
     }
 
     /**
-      * @Title: rank
-      * @Description: 
-      * @param @param key
-      * @param @param a
-      * @param @param lo
-      * @param @param hi
-      * @param @return   
-      * @return index of key in array a[] if present; -1 if not present
-      * @throws
-      *
-      * @Author xiaosonglin
-      * @Date Jan 16, 2015 3:27:17 PM
-      */
+     * @Title: rank
+     * @Description:
+     * @param @param key
+     * @param @param a
+     * @param @param lo
+     * @param @param hi
+     * @param @return
+     * @return index of key in array a[] if present; -1 if not present
+     * @throws
+     * 
+     * @Author xiaosonglin
+     * @Date Jan 16, 2015 3:27:17 PM
+     */
     public static int rank(int key, int[] a, int lo, int hi)
     {
         int mid = (lo + hi) / 2;
@@ -95,29 +109,31 @@ public class BinarySearch
     public static void main(String[] args)
     {
 
-        // read the integers from a file
-        In in = new In(args[0]);
-        StdOut.println(args[0]);
-        int[] whitelist = in.readAllInts();
-
-        // sort the array
-        Arrays.sort(whitelist);
-
-        // read integer key from standard input; print if not in whitelist
-//        while (!StdIn.isEmpty())
-//        {
-//            int key = StdIn.readInt();
-//            if (rank(key, whitelist) == -1)
-//                StdOut.println(key);
-//        }
-        while (!StdIn.isEmpty())
-        {
-            int key = StdIn.readInt();
-            if (rank(key, whitelist,0,whitelist.length-1) == -1)
-                StdOut.println(key);
-        }
-      
-        
+//         // read the integers from a file
+//         In in = new In(args[0]);
+//         StdOut.println(args[0]);
+//         int[] whitelist = in.readAllInts();
+//        
+//         // sort the array
+//         Arrays.sort(whitelist);
+//
+//         //read integer key from standard input; print if not in whitelist
+//         while (!StdIn.isEmpty())
+//         {
+//         int key = StdIn.readInt();
+//         if (rank(key, whitelist) == -1)
+//         StdOut.println(key);
+//         }
+         
+//         while (!StdIn.isEmpty())
+//         {
+//         int key = StdIn.readInt();
+//         if (rank(key, whitelist,0,whitelist.length-1) == -1)
+//         StdOut.println(key);
+//         }
+         
+         
+   
 
     }
 }
--- src/BinarySearchTest.java
+++ src/BinarySearchTest.java
@@ -1,7 +1,14 @@
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.Before;
 import org.junit.Test;
+
+import edu.princeton.cs.introcs.In;
+import edu.princeton.cs.introcs.StdOut;
 
 /**
  *
@@ -11,27 +18,53 @@ import org.junit.Test;
  */
 
 /**
- *@author xiaosonglin
- *@date Jan 16, 2015
+ * @author xiaosonglin
+ * @date Jan 16, 2015
  */
 public class BinarySearchTest
 {
 
+    In in;
+
+    In in2;
+
+    int[] whitelist;
+
     @Before
     public void setUp() throws Exception
     {
+        File file = new File("test/tinyT.txt");
+        in = new In(file);
+        whitelist = in.readAllInts();
+
+        Arrays.sort(whitelist);
+        File file2 = new File("test/tinyW.txt");
+        in2 = new In(file2);
+
     }
 
     @Test
     public void testRankIntIntArray()
     {
-        fail("Not yet implemented");
+
+        while (!in2.isEmpty())
+        {
+            int key = in2.readInt();
+            if (BinarySearch.rank(key, whitelist) == -1)
+                StdOut.println(key);
+        }
+
     }
 
     @Test
     public void testRankIntIntArrayIntInt()
     {
-        fail("Not yet implemented");
+        while (!in2.isEmpty())
+        {
+            int key = in2.readInt();
+            if (BinarySearch.rank(key, whitelist,0,whitelist.length-1) == -1)
+                StdOut.println(key);
+        }
     }
 
 }
